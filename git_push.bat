3bc4f80cac9cd67e2f7346d133adc0470c81f0aa
windrose <3506181262@qq.com>
2026-07-27 10:33:03 +0800

    auto save ~0,4~4,2~6,2dt:~8,2~10,2~12,2

--- git_push.bat
+++ git_push.bat
@@ -14,7 +14,17 @@ for /f "tokens=2 delims==" %%I in ('wmic os get localdatetime /value 2^>nul') do
 set "tag=%dt:~0,4%%dt:~4,2%%dt:~6,2%_%dt:~8,2%%dt:~10,2%%dt:~12,2%"
 
 echo 当前分支: %branch%
-echo 提交信息: auto save %tag%
+echo.
+
+:: 询问提交信息
+set "msg="
+set /p msg="输入提交信息（直接回车使用时间戳）: "
+
+:: 没输入就用时间戳
+if "%msg%"=="" set "msg=auto save %tag%"
+
+echo.
+echo 提交信息: %msg%
 echo.
 
 :: 生成临时脚本
